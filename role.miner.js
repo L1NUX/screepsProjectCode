123ac2fab3f5b1734a9cea475d3c487b76554db3
L1NUX <1337.h4x0r.me@gmail.com>
2019-06-06 13:18:08 -0700

    fixed a lot of bugs

--- role.miner.js
+++ role.miner.js
@@ -2,9 +2,12 @@ var roleMiner = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        var sources = creep.room.find(FIND_SOURCES);
+        console.log("working");
+        console.log(Game.getObjectById(creep.memory.source.id).pos.x);
+        console.log(Game.getObjectById(creep.memory.source.id).pos.y);
 
-        if(creep.harvest(Game.getObjectById(creep.memory.source.id)) == -9) {
+        if(creep.harvest(Game.getObjectById(creep.memory.source.id)) == ERR_NOT_IN_RANGE) {
+            console.log("working2");
             creep.moveTo(Game.getObjectById(creep.memory.source.id), {visualizePathStyle: {stroke: '#ffaa00'}});
         }
 	}
