419991e770ad20f9ad2e6aab2da0e951760d5ec0
L1NUX <1337.h4x0r.me@gmail.com>
2019-06-06 13:27:05 -0700

    more miner testing

--- role.miner.js
+++ role.miner.js
@@ -6,14 +6,9 @@ var roleMiner = {
         console.log(Game.getObjectById(creep.memory.source.id).pos.x);
         console.log(Game.getObjectById(creep.memory.source.id).pos.y);
 
-<<<<<<< HEAD
         if(creep.harvest(Game.getObjectById(creep.memory.source.id)) == ERR_NOT_IN_RANGE) {
             console.log("working2");
             creep.moveTo(Game.getObjectById(creep.memory.source.id), {visualizePathStyle: {stroke: '#ffaa00'}});
-=======
-        if(creep.harvest(creep.memory.source) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(creep.memory.source, {visualizePathStyle: {stroke: '#ffaa00'}});
->>>>>>> 92da019a849be25323b75059424d8e3e53fb483e
         }
 	}
 };
