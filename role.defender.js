355f3c4a90cd911360831650af6f2ee6c80be5dc
L1NUX <1337.h4x0r.me@gmail.com>
2019-06-06 19:05:11 -0700

    added harvester functionality to unused defenders

--- role.defender.js
+++ role.defender.js
@@ -13,7 +13,29 @@ var roleDefender = {
                 creep.moveTo(targets2[0], {visualizePathStyle: {stroke: '#1000ff'}});
             }
         } else{
-            creep.moveTo(creep.room.find(FIND_STRUCTURE_SPAWN)[0]);
+            if(creep.carry[RESOURCE_ENERGY] < creep.carryCapacity) {
+                var sources = creep.room.find(FIND_DROPPED_ENERGY);
+                //sources.push(creep.room.find(FIND_STRUCTURES, {filter: (structure) => {return structure.structureType == STRUCTURE_CONTAINER}}));
+    
+                if(creep.pickup(sources[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+                }
+            }
+            else {
+                var targets = creep.room.find(FIND_STRUCTURES, {
+                        filter: (structure) => {
+                            return (structure.structureType == STRUCTURE_EXTENSION ||
+                                    structure.structureType == STRUCTURE_SPAWN ||
+                                    structure.structureType == STRUCTURE_TOWER
+                                    ) && ((_.sum(structure.store) < structure.storeCapacity) || (structure.energy < structure.energyCapacity));
+                        }
+                });
+                if(targets.length > 0) {
+                    if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
+                }
+            }
         }
     }
 };
